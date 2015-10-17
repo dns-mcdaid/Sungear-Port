99bf6ae5ef1986f9367ebc47d3be5077de8ea4a5
Radhika Mattoo <rm3485@nyu.edu>
2015-10-16 22:52:35 -0400

    Hypergeometric Math File Started in JS

---
+++ HypergeometricDistribution.js
@@ -0,0 +1,9 @@
+//uses functions from: 
+// exception.NotPositiveException
+// exception.NotStrictlyPositiveException
+// exception.NumberIsTooLargeException
+// util.FastMath
+// random.RandomGenerator
+// random.Well19937c
+
+//extends AbstractIntegerDistribution
