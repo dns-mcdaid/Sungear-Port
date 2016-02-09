69e16f2e8692ae201a6962d231704fcedad0c322
Dennis McDaid <Dmcdaid95@gmail.com>
2016-02-09 03:20:26 -0500

    Build generic UI with dummy values.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+define(['jquery'], function(jq){
+
+  // Control table highlighter
+  $('#controls tbody tr').on('click', function(event) {
+      $(this).addClass('highlight').siblings().removeClass('highlight');
+  });
+
+  $('#genes tbody tr').on('click', function(event) {
+      $(this).addClass('highlight').siblings().removeClass('highlight');
+  });
+});
+
+
+
+/**
+HTML Canvas initialization
+
 var WIDTH = 800;
 var HEIGHT = 600;
 var clicked = false;
@@ -8,3 +25,4 @@ var context = canvas.getContext("2d");
 context.rect(0, 0, WIDTH, HEIGHT);
 context.fillStyle = "black";
 context.fill();
+*/
