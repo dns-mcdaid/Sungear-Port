446c3ea6e18e0574e9c78b53087e53667b8a9908
Radhika Mattoo <rm3485@nyu.edu>
2016-02-14 21:54:19 -0500

    more analysis translations

---
+++ hyperGeo/analysis/solvers/BaseAbstractUnivariateSolver.js
@@ -0,0 +1,44 @@
+/*
+Radhika Mattoo, February 2016 N.Y.
+
+Porting Sungear from Java to Javascript,
+Translated from Ilyas Mounaime's Java code
+
+*/
+//FIXME: how do you setup functions in js? is that possible?
+var DEFAULT_RELATIVE_ACCURACY = 1e-14;
+var DEFAULT_FUNCTION_VALUE_ACCURACY = 1e-15;
+var counterMax;
+var
+//implements BaseUnivariateSolver
+
+
+function BaseUnivariateSolver(relativeAccuracy, absoluteAccuracy, functionValueAccuracy){
+  this.evaluations = 0;
+  this.counterMax;
+  this.searchMin;
+  this.searchMax;
+  this.searchStart;
+
+  if(arguments.length == 1){
+    this.relativeAccuracy = DEFAULT_RELATIVE_ACCURACY;
+    this.absoluteAccuracy = absoluteAccuracy;
+    this.functionValueAccuracy = DEFAULT_FUNCTION_VALUE_ACCURACY;
+  }else if(arguments.length == 2){
+    this.relativeAccuracy = relativeAccuracy;
+    this.absoluteAccuracy = absoluteAccuracy;
+    this.functionValueAccuracy = DEFAULT_FUNCTION_VALUE_ACCURACY;
+  }else{
+    this.relativeAccuracy = relativeAccuracy;
+    this.absoluteAccuracy = absoluteAccuracy;
+    this.functionValueAccuracy = functionValueAccuracy;
+  }
+}
+
+BaseUnivariateSolver.prototype.getMaxEvaluations = function () {
+  return counterMax;
+};
+
+BaseUnivariateSolver.prototype.getEvaluations = function () {
+  return this.evaluations;
+};
