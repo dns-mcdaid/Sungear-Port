60451313cab4138faf19738adc6d28c8bd51d94c
Dennis McDaid <Dmcdaid95@gmail.com>
2016-03-25 17:14:37 -0400

    "Find Cool" Current expands the number of verticies

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,13 @@ function(anchor, anchorDisplay, p5, sungear, vesselDisplay){
   var makePolygon = false;
   var toDraw;
   var currentColor = sungear.C_HIGHLIGHT;
+  var sides = 1;
+  var HEIGHT;
+  var WIDTH;
   var setupCanvas = new p5(function(p5){
     p5.setup = function() {
-      var HEIGHT = document.getElementById('sunGui').clientHeight;
-      var WIDTH = document.getElementById('sunGui').clientWidth;
+      WIDTH = document.getElementById('sunGui').clientWidth;
+      HEIGHT = document.getElementById('sunGui').clientHeight;
       canvas = p5.createCanvas(WIDTH,HEIGHT);
       var testDis = new vesselDisplay.VesselDisplay("testAnc");
       var testShape = testDis.makeShape(4);
@@ -29,7 +32,7 @@ function(anchor, anchorDisplay, p5, sungear, vesselDisplay){
       }
 
       if(makePolygon) {
-        p5.polygon(100, 100, 70, 7);
+        p5.polygon(WIDTH/2, HEIGHT/2, HEIGHT/2-5, sides);
       }
 
       if (p5.mouseIsPressed) {
@@ -44,23 +47,28 @@ function(anchor, anchorDisplay, p5, sungear, vesselDisplay){
     },
 
     p5.polygon = function(x, y, radius, npoints) {
-      var angle = p5.TWO_PI / npoints;
-      p5.fill(0);
+      p5.fill(16,16,16);
       p5.stroke("#f00");
       p5.strokeWeight(4);
-      p5.beginShape();
-      for (var a = 0; a < p5.TWO_PI; a += angle) {
-        var sx = x + p5.cos(a) * radius;
-        var sy = y + p5.sin(a) * radius;
-        p5.vertex(sx, sy);
+      if (npoints > 2) {
+        var angle = p5.TWO_PI / npoints;
+        p5.beginShape();
+        for (var a = 0; a < p5.TWO_PI; a += angle) {
+          var sx = x + p5.cos(a) * radius;
+          var sy = y + p5.sin(a) * radius;
+          p5.vertex(sx, sy);
+        }
+        p5.endShape(p5.CLOSE);
+      } else {
+        p5.ellipse(x,y,HEIGHT-5,HEIGHT-5)
       }
-      p5.endShape(p5.CLOSE);
       p5.stroke(0);
       p5.strokeWeight(1);
     }
 
     document.getElementById("findCool").addEventListener("click", function(){
       currentColor = "#0ff";
+      sides++;
       makePolygon = true;
     });
 
